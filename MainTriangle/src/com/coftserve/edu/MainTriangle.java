e5423e6c107f1ec5b834c2ccd06fe9a75ec2f0ab
natanid <natanid@yandex.ua>
2017-05-02 15:11:53 +0300

    MainTriangle

---
+++ MainTriangle/src/com/coftserve/edu/MainTriangle.java
@@ -0,0 +1,19 @@
+package com.coftserve.edu;
+
+public class MainTriangle {
+
+public static void main(String[] args){
+
+int x1 = 3, y1 = 4, z1 = 5;
+
+int perimeter1 = x1 + y1 + z1;
+
+System.out.println("perimeter1 value = " + perimeter1);;
+
+int x2 = 6, y2 = 7, z2 = 8;
+
+int perimeter2 = x2 + y2 + z2;
+
+System.out.println("perimeter2 value = " + perimeter2);
+
+}}
